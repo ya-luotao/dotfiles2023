1a32ac316a3f5ae37aa038fd93e92a6c34cee501
ya-luotao <luotao@hey.com>
2023-09-19 18:55:21 +0800

    astronvim: disable filesystem.filtered_items.hide_gitignored of neo-tree

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -133,6 +133,9 @@ return {
         follow_current_file = { enabled = true },
         hijack_netrw_behavior = "open_current",
         use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_gitignored = false,
+        },
       },
       event_handlers = {
         {
